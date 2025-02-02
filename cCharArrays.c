c23de15cb0398080e3116360bc21300c6dfbc82e
Aduraseyistarstruck <osilajaaduraseyi@gmail.com>
2025-02-02 22:32:54 +0100

    string

---
+++ cCharArrays.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char string[] = {'H','e','l','l','o','.'};
+    for (int i = 0; i<6 ;++i){
+        printf("%c",string[i]);
+    }
+    printf("\n");
+    return 0;
+}
