b242608d8c8febb35f406b68a1ab37dd9b56a728
Aduraseyistarstruck <osilajaaduraseyi@gmail.com>
2025-02-03 23:53:46 +0100

    Checks if supplied character is A

---
+++ name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    char letter;
+    printf("ENTER YOUR LETTER: ");
+    scanf("%c", &letter);
+    if (letter == 'A'){
+        printf("Welcome Aduraseyi");
+    }
+    return 0;
+}
