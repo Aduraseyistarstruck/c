edcbccd55bbfd5ebd325119e484b7a2f5f816c70
Aduraseyistarstruck <osilajaaduraseyi@gmail.com>
2025-02-25 02:31:53 +0100

    modified

--- name.c
+++ name.c
@@ -7,10 +7,12 @@ int main(void){
         printf("Welcome Aduraseyi");
     }
     //Welcome Aduraseyi is printed if the letter is A
-    // char words[];
+    // char words[50];  you have to declare a size if you are predefining only
+    // char words[] = "Aduris";
     // printf("Enter your name: ");
     // scanf("%s",&words);
     // printf("%s",words[]);
     //visual studio code might bring errors even when there's no error, simply close terminal and try running again.
+
     return 0;
 }
